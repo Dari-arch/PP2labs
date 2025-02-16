ec4f478b87ba2763c046e9276c725fcc681b5d80
Dari-arch <dariya.05.07@icloud.com>
2025-02-16 20:34:11 +0600

    json file

---
+++ lab4/labka.py
@@ -0,0 +1,17 @@
+import json
+
+with open("sampledata.json", encoding="utf-8") as file:
+    data = json.load(file)
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':<50} {'Description':<20} {'Speed':<7} {'MTU':<7}")
+print("-" * 80)
+
+for item in data["imdata"]:
+    attributes = item["l1PhysIf"]["attributes"]
+    dn = attributes["dn"]
+    description = attributes.get("descr", "")
+    speed = attributes.get("speed", "inherit")
+    mtu = attributes.get("mtu", "")
+
+    print(f"{dn:<50} {description:<20} {speed:<7} {mtu:<7}")
